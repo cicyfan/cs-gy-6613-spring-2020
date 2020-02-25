a78ab4d1f2f2015837e3fe6cb966bc82b0d9f707
MONOGIOUDIS Pantelis <pantelis.monogioudis@nokia.com>
2020-02-25 07:23:30 -0500

    Publishing to gh-pages (publish.sh)

--- en.search.min.05b52673193eb736de8d4b242667a1aad92246ff60e3294c7c42d6f41962c8cd.js
+++ en.search.min.05b52673193eb736de8d4b242667a1aad92246ff60e3294c7c42d6f41962c8cd.js
@@ -2,7 +2,7 @@
 const characterPressed=String.fromCharCode(e.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();e.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/cs-gy-6613-spring-2020/flexsearch.min.js');loadScript('/cs-gy-6613-spring-2020/en.search-data.min.a6306e611948c902e1a00210b8d84fad16a29cd5a6e8f5c51f3df5fa9aa7236d.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/cs-gy-6613-spring-2020/flexsearch.min.js');loadScript('/cs-gy-6613-spring-2020/en.search-data.min.c0b876f7403de9e0d53662591369da68a335bee9cd335bab2f4bbcccd3e8d71e.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
